1ad92c37b7ac234952b3010fcc4e068ccdcd5e0f
Nancy M Schorr <nms@schorrmedia.com>
2020-07-01 06:15:04 +0200

    new edits for spe

---
+++ public/moreConsts.js
@@ -0,0 +1,13 @@
+
+    const INVOKE = "invokeView";
+    const CONTLABEL = "Content-Type";
+    const GCMETHOD = "getContract";
+    const CONTTYPE =  "application/json;charset=UTF-8";
+
+    const MULTIPLY = 100000000;
+
+    const OPTIONS = {
+      headers: {'X-Custom-Header': 'application/json;charset=UTF-8'}
+    };
+
+
