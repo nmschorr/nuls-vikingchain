0abb17590fbe765f06e2bce7103abf4d6a7c52d4
Nancy M Schorr <nms@schorrmedia.com>
2020-07-01 05:24:32 +0200

    new edits for spe

---
+++ public/constants.js
@@ -0,0 +1,9 @@
+const CONT_ADDY = "SPEXdKRT56gn8YfFy8DjSgQbkoVaTkn6XxrLL7";
+const SENDER =   "SPEXdKRT4pz7ZhasM9pTK4fvGrJf8eod5ZqtXa";
+const OWNER =  "SPEXdKRT4pz7ZhasM9pTK4fvGrJf8eod5ZqtXa";
+const BUYER =   "SPEXdKRT4nswCgHUv1YfuC9DK2u8etwpPbx8Np";
+const CHAINID_w = 4810;
+
+const POSTURL_w4 = "http://westteam.nulstar.com:8004/jsonrpc";
+const POSTURL_w3 = "http://westteam.nulstar.com:8003";
+
