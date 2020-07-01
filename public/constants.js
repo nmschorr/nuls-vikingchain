6df0ae3bc2912b790a8c6c3ca862a4fcd5c15537
nmschorr <12592910+nmschorr@users.noreply.github.com>
2020-07-01 06:50:27 +0100

    added script import

--- public/constants.js
+++ public/constants.js
@@ -1,18 +1,18 @@
-const CONT_ADDY = "SPEXdKRT56gn8YfFy8DjSgQbkoVaTkn6XxrLL7";
-const SENDER =   "SPEXdKRT4pz7ZhasM9pTK4fvGrJf8eod5ZqtXa";
-const OWNER =  "SPEXdKRT4pz7ZhasM9pTK4fvGrJf8eod5ZqtXa";
-const BUYER =   "SPEXdKRT4nswCgHUv1YfuC9DK2u8etwpPbx8Np";
-const CHAINID = 4810;
-const PW = "password123"
+export const CONT_ADDY = "SPEXdKRT56gn8YfFy8DjSgQbkoVaTkn6XxrLL7";
+export const SENDER =   "SPEXdKRT4pz7ZhasM9pTK4fvGrJf8eod5ZqtXa";
+export const OWNER =  "SPEXdKRT4pz7ZhasM9pTK4fvGrJf8eod5ZqtXa";
+export const BUYER =   "SPEXdKRT4nswCgHUv1YfuC9DK2u8etwpPbx8Np";
+export const CHAINID = 4810;
+export const PW = "password123"
 
-const VALUE_ASSET = 25;
-const GAS_PRICE = 25;
-const GAS_LIMIT = 9000000;
+export const VALUE_ASSET = 25;
+export const GAS_PRICE = 25;
+export const GAS_LIMIT = 9000000;
 
-const REMARKS = "Space Exploration Product Review";
+export const REMARKS = "Space Exploration Product Review";
 
 
-const POSTURL_w4 = "http://westteam.nulstar.com:8004/jsonrpc";
-const POSTURL_w3 = "http://westteam.nulstar.com:8003";
+export const POSTURL_w4 = "http://westteam.nulstar.com:8004/jsonrpc";
+export const POSTURL_w3 = "http://westteam.nulstar.com:8003";
 
-const ID_D = 900092;
+export const ID_D = 900092;
