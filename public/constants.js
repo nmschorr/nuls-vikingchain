729518d23566bd8cda6c4c77a0be29ef4cc62933
nmschorr <12592910+nmschorr@users.noreply.github.com>
2020-07-01 06:01:29 +0100

    added constants

--- public/constants.js
+++ public/constants.js
@@ -2,20 +2,17 @@ const CONT_ADDY = "SPEXdKRT56gn8YfFy8DjSgQbkoVaTkn6XxrLL7";
 const SENDER =   "SPEXdKRT4pz7ZhasM9pTK4fvGrJf8eod5ZqtXa";
 const OWNER =  "SPEXdKRT4pz7ZhasM9pTK4fvGrJf8eod5ZqtXa";
 const BUYER =   "SPEXdKRT4nswCgHUv1YfuC9DK2u8etwpPbx8Np";
-const CHAINID_w = 4810;
+const CHAINID = 4810;
+const PW = "password123"
+
+const VALUE_ASSET = 25;
+const GAS_PRICE = 25;
+const GAS_LIMIT = 9000000;
+
+const REMARKS = "Space Exploration Product Review";
+
 
 const POSTURL_w4 = "http://westteam.nulstar.com:8004/jsonrpc";
 const POSTURL_w3 = "http://westteam.nulstar.com:8003";
 
-let VALUE_ASSET = 25;
-let BIG_VALUE = MULTIPLY * VALUE_ASSET;
-let PRODUCTID_REVIEW = [this.selectedProductId, this.review];
-let DESCRIPTION = "(String productId, String reviewComments) return LReviewContract$Review;";
-let METHOD_D = "contractCall";
-let REQUEST_TYPE = "writeReview";
-let ID_D = 900092;
-let GAS_PRICE = 25;
-let PW = "password123"
-let REMARKS = "Space Exploration Product Review";
-let GAS_LIMIT = 9000000;
-
+const ID_D = 900092;
