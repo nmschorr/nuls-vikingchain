3700c3a67a29e89243ccbfb7ea521316a9633438
Nancy M Schorr <nms@schorrmedia.com>
2020-07-01 05:44:20 +0200

    more change

--- public/constants.js
+++ public/constants.js
@@ -7,3 +7,15 @@ const CHAINID_w = 4810;
 const POSTURL_w4 = "http://westteam.nulstar.com:8004/jsonrpc";
 const POSTURL_w3 = "http://westteam.nulstar.com:8003";
 
+let VALUE_ASSET = 25;
+let BIG_VALUE = MULTIPLY * VALUE_ASSET;
+let PRODUCTID_REVIEW = [this.selectedProductId, this.review];
+let DESCRIPTION = "(String productId, String reviewComments) return LReviewContract$Review;";
+let METHOD_D = "contractCall";
+let REQUEST_TYPE = "writeReview";
+let ID_D = 900092;
+let GAS_PRICE = 25;
+let PW = "password123"
+let REMARKS = "Space Exploration Product Review";
+let GAS_LIMIT = 9000000;
+
