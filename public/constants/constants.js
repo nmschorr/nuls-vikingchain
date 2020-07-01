9a845083042fb452d1e732e7828560e38a1eb286
nmschorr <12592910+nmschorr@users.noreply.github.com>
2020-07-01 10:47:44 +0100

    added script import

--- public/constants/constants.js
+++ public/constants/constants.js
@@ -1,13 +1,17 @@
-export const CONT_ADDY = "SPEXdKRT56gn8YfFy8DjSgQbkoVaTkn6XxrLL7";
-export const SENDER =   "SPEXdKRT4pz7ZhasM9pTK4fvGrJf8eod5ZqtXa";
-export const OWNER =  "SPEXdKRT4pz7ZhasM9pTK4fvGrJf8eod5ZqtXa";
-export const BUYER =   "SPEXdKRT4nswCgHUv1YfuC9DK2u8etwpPbx8Np";
-export const CHAINID = 4810;
-export const PW = "password123"
+var CHAINID = 4810;
+var PW = "password123"
+var CONT_ADDY = "SPEXdKRT56gn8YfFy8DjSgQbkoVaTkn6XxrLL7";
+var SENDER =   "SPEXdKRT4pz7ZhasM9pTK4fvGrJf8eod5ZqtXa";
+var OWNER =  "SPEXdKRT4pz7ZhasM9pTK4fvGrJf8eod5ZqtXa";
+var BUYER =   "SPEXdKRT4nswCgHUv1YfuC9DK2u8etwpPbx8Np";
 
-export const VALUE_ASSET = 25;
-export const GAS_PRICE = 25;
-export const GAS_LIMIT = 9000000;
+var VALUE_ASSET = 25;
+var GAS_PRICE = 25;
+var GAS_LIMIT = 9000000;
 
-export const POSTURL_w4 = "http://westteam.nulstar.com:8004/jsonrpc";
-export const POSTURL_w3 = "http://westteam.nulstar.com:8003";
+var POSTURL_w3 = "http://westteam.nulstar.com:8003";
+var POSTURL_w4 = "http://westteam.nulstar.com:8004/jsonrpc";
+
+export var constantsObj = {
+    CHAINID, PW, CONT_ADDY, SENDER, OWNER, BUYER, 
+    VALUE_ASSET, GAS_PRICE, GAS_LIMIT, POSTURL_w3, POSTURL_w4 }
